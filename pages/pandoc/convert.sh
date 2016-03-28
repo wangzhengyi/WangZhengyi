2c2fd33894a95d6fb54db6ae4d28e056848660d0
wangzhengyi <wzyll1314@gmail.com>
2016-03-28 11:20:21 +0800

    修改格式

--- pages/pandoc/convert.sh
+++ pages/pandoc/convert.sh
@@ -4,4 +4,4 @@ pandoc pandoc.markdown -t html5 -o index.html --toc --smart --template=pm-templa
 pandoc pandoc.markdown -o pandoc-zhtw.pdf --toc --smart --template=pm-template --latex-engine=xelatex -V mainfont='WenQuanYi Zen Hei Mono'
 
 # add style to table
-sed -i '' 's/<table>/<table class="table table-bordered table-condensed">/' index.html
+#sed -i '' 's/<table>/<table class="table table-bordered table-condensed">/' index.html
